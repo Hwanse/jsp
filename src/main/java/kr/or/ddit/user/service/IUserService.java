ae649834fb8c1d2aad024ee96e8405e09b9e42e4
Kimhs <khsb2012@gmail.com>
2019-05-23 17:06:21 +0900

    사용자 리스트 화면 개발
    	- userService : 임의의 가짜 값을 리턴
    		(추후 수정 예정)

---
+++ src/main/java/kr/or/ddit/user/service/IUserService.java
@@ -0,0 +1,19 @@
+package kr.or.ddit.user.service;
+
+import java.util.List;
+
+import kr.or.ddit.user.model.UserVO;
+
+public interface IUserService {
+
+	/**
+	 * 
+	* Method : userList
+	* 작성자 : PC14
+	* 변경이력 :
+	* @return
+	* Method 설명 : 사용자 전체리스트 조회
+	 */
+	List<UserVO> userList();
+	
+}
